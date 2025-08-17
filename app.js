cf734c1cff209ff5d13cc25dceac8ece1108d049
yashrepogit <yas@cloudify.biz>
2025-08-17 16:31:30 +0530

    added new hosted link

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ import AuthRouter from './Routes/Auth.js';
 app.use(cookieParser());
 // ✅ CORS
 const corsOptions = {
-    origin: ["https://team-collab-dusky.vercel.app","http://localhost:3000"],
+    origin: ["https://team-collab-sable.vercel.app","http://localhost:3000"],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
 };
